cf508195b18cebc1c3e00b3b52d7d421fa104815
imane himmi <imane.himmi23@gmail.com>
2025-12-19 18:58:25 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'courses-platform',
+  webDir: 'www'
+};
+
+export default config;
